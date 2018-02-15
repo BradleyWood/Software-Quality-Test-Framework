aa808b182026af8c50eb09561b3814a38cc21711
BradleyWood <bradley.wood@uoit.net>
2018-02-15 17:50:01 -0500

    basic assertions

---
+++ src/main/java/org/sqtf/assertions/Assert.java
@@ -0,0 +1,32 @@
+package org.sqtf.assertions;
+
+import java.util.Objects;
+
+public class Assert {
+
+    public static void fail(String message) {
+        throw new AssertionError(message);
+    }
+
+    public static void assertTrue(boolean expression) {
+        if (!expression)
+            fail("Expression must evaluate to true");
+    }
+
+    public static void assertFalse(boolean expression) {
+        if (expression)
+            fail("Expression must evaluate to true");
+    }
+
+    public static void assertEquals(Object expected, Object actual) {
+        if (!Objects.equals(expected, actual)) {
+            fail("Expected value " + expected.toString() + " but got " + actual);
+        }
+    }
+
+    public static void assertNotEqual(Object a, Object b) {
+        if (Objects.equals(a, b)) {
+            fail(a.toString() + " should not equal to " + b);
+        }
+    }
+}

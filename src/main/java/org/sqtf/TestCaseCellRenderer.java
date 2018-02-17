baf410c8898a5247f9d41e75902f45a4f65a24db
BradleyWood <bradley.wood@uoit.net>
2018-02-17 17:26:20 -0500

    model and renderer for tree nodes

---
+++ src/main/java/org/sqtf/TestCaseCellRenderer.java
@@ -0,0 +1,54 @@
+package org.sqtf;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeCellRenderer;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+final class TestCaseCellRenderer implements TreeCellRenderer {
+
+    private JLabel label;
+    private ImageIcon pass;
+    private ImageIcon fail;
+    private ImageIcon inProgress;
+
+    TestCaseCellRenderer() {
+        label = new JLabel();
+        try {
+            pass = new ImageIcon(ImageIO.read(new File("checkmark.png")), "Test Passed");
+            fail = new ImageIcon(ImageIO.read(new File("fail.png")), "Test Failed");
+            inProgress = new ImageIcon(ImageIO.read(new File("progress.gif")), "Waiting");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected,
+                                                  boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        DefaultMutableTreeNode node = ((DefaultMutableTreeNode) value);
+        Object o = node.getUserObject();
+        if (o instanceof TestCase) {
+            TestCase tc = (TestCase) o;
+            label.setText(tc.getName() == null ? tc.getOwner() : tc.getName());
+
+            switch (tc.getTestStatus()) {
+                case PASSED:
+                    label.setIcon(pass);
+                    break;
+                case FAILED:
+                    label.setIcon(fail);
+                    break;
+                case WAITING:
+                    label.setIcon(inProgress);
+                    break;
+            }
+            return label;
+        }
+        label.setText(value.toString());
+        return label;
+    }
+}

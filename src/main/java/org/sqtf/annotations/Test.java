5ab5ba1f93f90ccc82bbac866c102b597ce68fdd
BradleyWood <bradley.wood@uoit.net>
2018-02-18 14:26:31 -0500

    documentation

--- src/main/java/org/sqtf/annotations/Test.java
+++ src/main/java/org/sqtf/annotations/Test.java
@@ -8,8 +8,21 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
 
+    /**
+     * Specify any expected exception to be thrown by the test. If the test
+     * does not throw this exception type or throws a different exception
+     * the test will be marked as failure.
+     *
+     * @return A class representing a Throwable type
+     */
     @NotNull Class<? extends Throwable> expected() default NoException.class;
 
+    /**
+     * Specify a timeout for the test. Causes test failure if the test does
+     * not complete before the timeout.
+     *
+     * @return the timeout measured in milliseconds
+     */
     int timeout() default 0;
 
     class NoException extends Throwable {
--- src/main/java/org/sqtf/assertions/Assert.java
+++ src/main/java/org/sqtf/assertions/Assert.java
@@ -4,30 +4,59 @@ import java.util.Objects;
 
 public final class Assert {
 
+    /**
+     * Causes test failure with no message
+     */
     public static void fail() {
         throw new AssertionError();
     }
 
+    /**
+     * Causes test failre with a message
+     * @param message The reason for test failure
+     */
     public static void fail(String message) {
         throw new AssertionError(message);
     }
 
+    /**
+     * Asserts that the expression is true. If it is not
+     * the test will fail
+     * @param expression
+     */
     public static void assertTrue(boolean expression) {
         if (!expression)
             fail("Expression must evaluate to true");
     }
 
+    /**
+     * Asserts that the expression is false. If it is not
+     * the test will fail
+     * @param expression The boolean expression to test
+     */
     public static void assertFalse(boolean expression) {
         if (expression)
             fail("Expression must evaluate to true");
     }
 
+    /**
+     * Asserts that the two objects are equal. If they are not
+     * the test will fail
+     * @param expected The expected value
+     * @param actual The actual value
+     */
     public static void assertEquals(Object expected, Object actual) {
         if (!Objects.equals(expected, actual)) {
             fail("Expected value " + expected.toString() + " but got " + actual);
         }
     }
 
+    /**
+     * Asserts that the two objects are equal. If they are not
+     * the test will fail
+     * @param a The first object
+     * @param b The second object
+     */
     public static void assertNotEqual(Object a, Object b) {
         if (Objects.equals(a, b)) {
             fail(a.toString() + " should not equal to " + b);

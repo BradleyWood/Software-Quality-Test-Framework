7c415f32e986e60eda3c0cfddf82eae0876e92e0
BradleyWood <bradley.wood@uoit.net>
2018-03-04 13:43:55 -0500

    assertions with descriptive messages

--- src/main/java/org/sqtf/assertions/Assert.java
+++ src/main/java/org/sqtf/assertions/Assert.java
@@ -13,6 +13,7 @@ public final class Assert {
 
     /**
      * Causes test failre with a message
+     *
      * @param message The reason for test failure
      */
     public static void fail(String message) {
@@ -22,6 +23,7 @@ public final class Assert {
     /**
      * Asserts that the expression is true. If it is not
      * the test will fail
+     *
      * @param expression
      */
     public static void assertTrue(boolean expression) {
@@ -30,8 +32,21 @@ public final class Assert {
     }
 
     /**
+     * Asserts that the expression is true. If it is not
+     * the test will fail
+     *
+     * @param expression The boolean expression to test
+     * @param message    The message to report on failure
+     */
+    public static void assertTrue(boolean expression, String message) {
+        if (!expression)
+            fail(message);
+    }
+
+    /**
      * Asserts that the expression is false. If it is not
      * the test will fail
+     *
      * @param expression The boolean expression to test
      */
     public static void assertFalse(boolean expression) {
@@ -40,10 +55,23 @@ public final class Assert {
     }
 
     /**
+     * Asserts that the expression is false. If it is not
+     * the test will fail
+     *
+     * @param expression The boolean expression to test
+     * @param message    The message to report on failure
+     */
+    public static void assertFalse(boolean expression, String message) {
+        if (expression)
+            fail(message);
+    }
+
+    /**
      * Asserts that the two objects are equal. If they are not
      * the test will fail
+     *
      * @param expected The expected value
-     * @param actual The actual value
+     * @param actual   The actual value
      */
     public static void assertEquals(Object expected, Object actual) {
         if (!Objects.equals(expected, actual)) {
@@ -54,12 +82,41 @@ public final class Assert {
     /**
      * Asserts that the two objects are equal. If they are not
      * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param message  The message to report on failure
+     */
+    public static void assertEquals(Object expected, Object actual, String message) {
+        if (!Objects.equals(expected, actual)) {
+            fail(message);
+        }
+    }
+
+    /**
+     * Asserts that the two objects are equal. If they are not
+     * the test will fail
+     *
      * @param a The first object
      * @param b The second object
      */
     public static void assertNotEqual(Object a, Object b) {
         if (Objects.equals(a, b)) {
             fail(a.toString() + " should not equal to " + b);
+        }
+    }
+
+    /**
+     * Asserts that the two objects are equal. If they are not
+     * the test will fail
+     *
+     * @param a       The first object
+     * @param b       The second object
+     * @param message The message to report on failure
+     */
+    public static void assertNotEqual(Object a, Object b, String message) {
+        if (Objects.equals(a, b)) {
+            fail(message);
         }
     }
 }

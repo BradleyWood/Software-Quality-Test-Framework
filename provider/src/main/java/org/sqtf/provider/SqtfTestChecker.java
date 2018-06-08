3b89f434997d5b8541d02a90ee9f5073f4170187
BradleyWood <bradley.wood@uoit.net>
2018-06-08 18:46:38 -0400

    verify that a class is a test class

---
+++ provider/src/main/java/org/sqtf/provider/SqtfTestChecker.java
@@ -0,0 +1,16 @@
+package org.sqtf.provider;
+
+import org.apache.maven.surefire.util.ScannerFilter;
+import org.sqtf.annotations.Test;
+
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
+public class SqtfTestChecker implements ScannerFilter {
+
+    @Override
+    public boolean accept(final Class testClass) {
+        return Arrays.stream(testClass.getDeclaredMethods()).filter(m -> m.getAnnotation(Test.class) != null)
+                .anyMatch(m -> !Modifier.isStatic(m.getModifiers()) && Modifier.isPublic(m.getModifiers()));
+    }
+}

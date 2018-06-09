1ad79809c96e8cb181a25d757757965f6e3dfb5f
BradleyWood <bradley.wood@uoit.net>
2018-06-09 13:21:12 -0400

    provide float, double, int, null assertion checks

--- core/src/main/java/org/sqtf/assertions/Assert.java
+++ core/src/main/java/org/sqtf/assertions/Assert.java
@@ -94,6 +94,85 @@ public final class Assert {
     }
 
     /**
+     * Asserts that the two integers are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     */
+    public static void assertEquals(int expected, int actual) {
+        assertEquals(expected, actual, "Expected value " + expected + " but got " + actual);
+    }
+
+    /**
+     * Asserts that the two integers are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param message  The message to report on failure
+     */
+    public static void assertEquals(int expected, int actual, String message) {
+        if (expected != actual) {
+            fail(message);
+        }
+    }
+
+    /**
+     * Asserts that the two floats are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param delta    The maximum amount that expected and actual values may deviate by
+     */
+    public static void assertEquals(float expected, float actual, float delta) {
+        assertEquals(expected, actual, delta, "Expected " + expected + " +- " + delta + " but got " + actual);
+    }
+
+    /**
+     * Asserts that the two floats are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param delta    The maximum amount that expected and actual values may deviate by
+     * @param message  The message to report on failure
+     */
+    public static void assertEquals(float expected, float actual, float delta, String message) {
+        if (expected + delta < actual || expected - delta > actual) {
+            fail(message);
+        }
+    }
+
+    /**
+     * Asserts that the two double values are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param delta    The maximum amount that expected and actual values may deviate by
+     */
+    public static void assertEquals(double expected, double actual, double delta) {
+        assertEquals(expected, actual, delta, "Expected " + expected + " +- " + delta + " but got " + actual);
+    }
+
+    /**
+     * Asserts that the two double values are equal. If they are not
+     * the test will fail
+     *
+     * @param expected The expected value
+     * @param actual   The actual value
+     * @param delta    The maximum amount that expected and actual values may deviate by
+     * @param message  The message to report on failure
+     */
+    public static void assertEquals(double expected, double actual, double delta, String message) {
+        if (expected + delta < actual || expected - delta > actual) {
+            fail(message);
+        }
+    }
+
+    /**
      * Asserts that the two objects are equal. If they are not
      * the test will fail
      *
@@ -116,6 +195,52 @@ public final class Assert {
      */
     public static void assertNotEqual(Object a, Object b, String message) {
         if (Objects.equals(a, b)) {
+            fail(message);
+        }
+    }
+
+    /**
+     * Asserts that the input object is null. Otherwise the
+     * test will fail
+     *
+     * @param value The value to test
+     */
+    public static void assertNull(Object value) {
+        assertNull(value, "Expected null but got " + value);
+    }
+
+    /**
+     * Asserts that the input object is null. Otherwise the
+     * test will fail
+     *
+     * @param value   The value to test
+     * @param message The message to report on failure
+     */
+    public static void assertNull(Object value, String message) {
+        if (value != null) {
+            fail(message);
+        }
+    }
+
+    /**
+     * Asserts that the input object is null. Otherwise the
+     * test will fail
+     *
+     * @param value The value to test
+     */
+    public static void assertNotNull(Object value) {
+        assertNotNull(value, "Expected non-null value");
+    }
+
+    /**
+     * Asserts that the input object is null. Otherwise the
+     * test will fail
+     *
+     * @param value   The value to test
+     * @param message The message to report on failure
+     */
+    public static void assertNotNull(Object value, String message) {
+        if (value == null) {
             fail(message);
         }
     }

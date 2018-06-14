95ea3de39320c308deb045d52516603284da9cda
BradleyWood <bradley.wood@uoit.net>
2018-06-13 22:44:49 -0400

    make data utilities package private

--- sqtf-core/src/main/java/org/sqtf/data/DataUtils.java
+++ sqtf-core/src/main/java/org/sqtf/data/DataUtils.java
@@ -8,10 +8,10 @@ import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DataUtils {
+class DataUtils {
 
     @Nullable
-    public static Object toType(@Nullable final Object input, @NotNull final Class<?> type) {
+    static Object toType(@Nullable final Object input, @NotNull final Class<?> type) {
         if (type.equals(int.class) || type.equals(Integer.class)) {
             return toInt(input);
         } else if (type.equals(float.class) || type.equals(Float.class)) {
@@ -43,7 +43,7 @@ public class DataUtils {
     }
 
     @Nullable
-    public static Number toNumber(@Nullable final Object obj){
+    private static Number toNumber(@Nullable final Object obj){
         if (obj instanceof String) {
             try {
                 return NumberFormat.getInstance().parse((String) obj);
@@ -57,49 +57,49 @@ public class DataUtils {
     }
 
     @Nullable
-    public static Integer toInt(@Nullable final Object obj) {
+    static Integer toInt(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.intValue();
     }
 
     @Nullable
-    public static Long toLong(@Nullable final Object obj) {
+    static Long toLong(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.longValue();
     }
 
     @Nullable
-    public static Short toShort(@Nullable final Object obj) {
+    static Short toShort(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.shortValue();
     }
 
     @Nullable
-    public static Byte toByte(@Nullable final Object obj) {
+    static Byte toByte(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.byteValue();
     }
 
     @Nullable
-    public static Float toFloat(@Nullable final Object obj) {
+    static Float toFloat(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.floatValue();
     }
 
     @Nullable
-    public static Double toDouble(@Nullable final Object obj) {
+    static Double toDouble(@Nullable final Object obj) {
         final Number number = toNumber(obj);
 
         return number == null ? null : number.doubleValue();
     }
 
     @Nullable
-    public static Boolean toBoolean(@Nullable final Object obj) {
+    static Boolean toBoolean(@Nullable final Object obj) {
         if (obj instanceof Boolean)
             return (Boolean) obj;
         if (obj instanceof String)
@@ -108,7 +108,7 @@ public class DataUtils {
     }
 
     @Nullable
-    public static Character toCharacter(@Nullable final Object obj) {
+    static Character toCharacter(@Nullable final Object obj) {
         if (obj instanceof Character)
             return (Character) obj;
         if (obj instanceof String) {
@@ -121,7 +121,7 @@ public class DataUtils {
     }
 
     @NotNull
-    public static String toString(@Nullable final Object obj) {
+    private static String toString(@Nullable final Object obj) {
         return String.valueOf(obj);
     }
 
